2257419860c715bb32fd2168b86a3def8beb3c3e
Gustavo Rodriguez <gustavojrs01@gmail.com>
2023-03-15 22:48:39 -0300

    async await

--- retos2.js
+++ retos2.js
@@ -1,10 +1,35 @@
-const cronometro = ()=>{
-    let cont = 1
-    setInterval(()=>{
-    
-        console.log(cont)
-        cont++
-    },1000)
+const data = [
+    {
+        id:1,
+        name:'Gustavo'
+    },
+    {
+        id:2,
+        name:'Victoria'
+    }
+]
+
+
+const getData = () => new Promise((resolve, reject)=>{
+    if (data.length === 0) {
+        reject(new Error('Data is empty'))
+    }
+    setTimeout(()=>{
+        resolve(data)
+    }, 2000)
+})
+
+// getData()
+//     .then((response)=>{
+//         console.log(response)
+//     })
+//     .catch((error)=>{
+//         console.log(error.message)
+//     })
+
+async function fetchedData(){
+    const personas = await getData()
+    console.log(personas)
 }
 
-cronometro()
+fetchedData()
