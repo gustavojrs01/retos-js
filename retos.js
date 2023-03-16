3b0f6f5d5f6285d31eea5f242002cace002e0e64
Gustavo Rodriguez <gustavojrs01@gmail.com>
2023-03-16 20:27:22 -0300

    async-await

--- retos.js
+++ retos.js
@@ -345,3 +345,41 @@
 
 // mostrarPersonas()
 // buscar('Gustavo')
+/***************
+ * ASYNC-AWAIT *
+ ***************/
+// const data = [
+//     {
+//         id:1,
+//         name:'Gustavo'
+//     },
+//     {
+//         id:2,
+//         name:'Victoria'
+//     }
+// ]
+
+
+// const getData = () => new Promise((resolve, reject)=>{
+//     if (data.length === 0) {
+//         reject(new Error('Data is empty'))
+//     }
+//     setTimeout(()=>{
+//         resolve(data)
+//     }, 2000)
+// })
+
+// // getData()
+// //     .then((response)=>{
+// //         console.log(response)
+// //     })
+// //     .catch((error)=>{
+// //         console.log(error.message)
+// //     })
+
+// async function fetchedData(){
+//     const personas = await getData()
+//     console.log(personas)
+// }
+
+// fetchedData()
