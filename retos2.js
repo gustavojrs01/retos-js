f6695bb9257a684a001ae33fc90d3feea25b247b
Gustavo Rodriguez <gustavojrs01@gmail.com>
2023-03-21 18:16:56 -0300

    NUevo check

--- retos2.js
+++ retos2.js
@@ -1,29 +1,26 @@
-const products = [
-    { name: "product1", price: 10, inStock: true },
-    { name: "product2", price: 20, inStock: false },
-    { name: "product3", price: 15, inStock: true },
-    { name: "product4", price: 5, inStock: false },
-  ]
+const hotel = hotelSystem(10);
 
-function sortByAvailabilityAndPrice(products) {     
-    return [...products].sort((a,b)=>a.price - b.price).sort((a,b)=>b.inStock - a.inStock)
+hotel.addReservation({
+  id: 1,
+  name: "John Doe",
+  checkIn: "01/01",
+  checkOut: "02/01",
+  roomNumber: 1,
+});
+
+hotel.addReservation({
+  id: 2,
+  name: "Pepe Doe",
+  checkIn: "01/01",
+  checkOut: "02/01",
+  roomNumber: 7,
+});
+
+
+function hotelSystem(rooms) {
+   return{
+    const searchReservation = (id)=>{
+        console.log('Hola')
+    }
+   }
 }
-// function sortByAvailabilityAndPrice(products) {
-//     let newList = [...products]    
-//     newList.sort((a,b)=>a.price - b.price)
-//     newList.sort((a,b)=>b.inStock - a.inStock) 
-//     return newList
-// }
-
-
-
-
-// function sortByAvailabilityAndPrice(products) {
-//     // Tu código aquí 👈
-//     const productsSorted = [...products];
-//     productsSorted.sort((a, b) => a.price - b.price)
-//     productsSorted.sort((a, b) => b.inStock - a.inStock)
-//     return productsSorted;
-//   }
-
-  console.log(sortByAvailabilityAndPrice(products))
